6095de27aba71dfaffa70dfa32d06122b0d36f0d
hawoon_an <hawoon.an@nhnent.com>
2019-01-11 17:02:07 +0900

    added

--- main.c
+++ main.c
@@ -36,8 +36,8 @@ int main(int argc, char* argv[])
  
 int add(int number1, int number2)
 {
-	result = number1+number2;
-	return result;
+	//TODO implementation
+	return 0;
 }
  
 int sub(int number1, int number2)
@@ -48,11 +48,11 @@ int sub(int number1, int number2)
 int multi(int number1, int number2)
 {
 	//TODO implementation
-	return number1* number2;
+	return 0;
 }
  
 int div(int number1, int number2)
 {
-	result = number1/number2;
-	return result;
+	//TODO implementation
+	return 0;
 }
