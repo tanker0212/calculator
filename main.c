e37385c19c3eea3049d7ce3717d3f0043056e2b0
정재원 <jgd0948@naver.com>
2019-01-11 17:17:15 +0900

    수식입려 ㄱㄱㄱ

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression: ");
+	printf("수식입력 ㄱㄱ: ");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
